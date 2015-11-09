81903f3e3a84dad51e1af14922e50b3923f0e4d1
Víctor Cea Paredes <vicea@alumnos.ubiobio.cl>
2015-11-09 15:39:08 -0300

    Conexion a la BD local

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
+    'dsn' => 'sqlsrv:Server=VÍCTOR-PC;Database=Normalizacion',
+    'username' => '',
     'password' => '',
     'charset' => 'utf8',
 ];
